6f41174c0b2fc96a575bdfcc6ccb4ea53638b3e7
Vojta Halmazňa <vojtech.halmazna@student.mgvsetin.cz>
2026-09-17 14:29:20 +0200

    feat: přidá pozdrav podle denní doby

--- greetings.py
+++ greetings.py
@@ -1,33 +1,23 @@
-# -*- coding: utf-8 -*-
-# Příliš žluťoučký kůň úpěl ďábelské ódy - testovací pangram
-"""_summary_
-Na inputu jméno, příjmení. Na výstupu jeden ze 3 možných pozdravů včetně vstupních informací.
-* jak vyčistit terminál
-* jak skutečně zajistit náhodnost
-* pozdrav podle denní doby
-"""
 
-##############################################################
-### Jak vymazat terminál před opětovným spuštěním - cls pro Win, clear pro Unix-like systémy
+# vymazání terminálu
 
 import os
+import random
+
 
 # Vymazání obrazovky terminálu (Windows)
 os.system("cls")
 
-
-##############################################################
 ### Základní verze - vždy stejná odpověď
-
 # Získání jména a příjmení od uživatele
+
 jmeno = input("Vojta")
 prijmeni = input("Halmazňa")
 
 
 # Generování pozdravu bez náhodného prvku a zobrazení v terminálu
-print(f"Ahoj, {jmeno} {prijmeni}! Těší mě, že tě poznávám.")
+#    print(f"Ahoj, {jmeno} {prijmeni}! Těší mě, že tě poznávám.")
 
-##############################################################
 ### Rozšířená verze - pseudonáhodný výběr bez zamíchání
 # vytvořit greetings jako list pozdravů
 
@@ -36,27 +26,21 @@ greetings = [f"Ahoj, {jmeno} {prijmeni}! Těší mě, že tě poznávám.",
              f"Zdar, {jmeno} {prijmeni}! Rád tě vidím."
              f"Bonjour, {jmeno} {prijmeni}! Comment ça va?"]
 
-import random
+
 print(random.choice(greetings))
 
+# Rozšířená verze - random seed()
 
-
-import random
-
-
-
-##############################################################
-### Rozšířená verze - random seed()
-
-# zamíchání, někdy se také používá s knihovnou time: inicializace seed pomocí time: random.seed(time.time())
 # side effect provedení v této části kódu má za následek i zamíchání volby při opětovném volání
 
-random.seed()
 
+# *verze - pozdrav podle denní doby
+from datetime import datetime
+hodina = datetime.now().hour
 
-
-##############################################################
-### *verze - pozdrav podle denní doby
-
-import datetime
-
+if 5 <= hodina < 11:
+    print(f"Dobré ráno, {jmeno} {prijmeni}! Je {hodina} hodin. ")
+elif 11 <= hodina < 17:
+    print(f"Kvalitní den přeji, {jmeno} {prijmeni}! Je {hodina} hodin. ")
+else:
+    print(f"Klidný večer nebo noc, {jmeno} {prijmeni}! Je {hodina} hodin. ")
